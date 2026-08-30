a7a45039b035e751f9b33747b2b92d3b879538cf
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2026-08-30 21:23:47 +0800

    Initialize member variables

--- chip8.h
+++ chip8.h
@@ -8,21 +8,20 @@ public:
     chip8();
 
 public:
-    uint8_t registers[16];
-    uint8_t memory[4096];
-    uint16_t index;
-    uint16_t programCounter;
-    uint16_t stack[16];
-    uint8_t stackPointer;
-    uint8_t delatTimer;
-    uint8_t soundTimer;
-    uint8_t keypad[16];
-    uint32_t displayBuffer[64 * 32];
+    uint8_t registers[16]{};
+    uint8_t memory[4096]{};
+    uint16_t index{};
+    uint16_t programCounter{};
+    uint16_t stack[16]{};
+    uint8_t stackPointer{};
+    uint8_t delatTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[16]{};
+    uint32_t displayBuffer[64 * 32]{};
 
-    uint16_t opcode; // $AD22 - first byte = op, second byte = data
+    uint16_t opcode{}; // $AD22 - first byte = op, second byte = data
 
-
-    void load_rom(const char* const fileName);
+    void load_rom(const char *const fileName);
 
 private:
     std::default_random_engine randomNumberGenerator;
