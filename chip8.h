70ed68cfc408a118a1b0a5110635f07f8daf6cbb
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2026-08-31 12:45:13 +0800

    Added Spec guide

--- chip8.h
+++ chip8.h
@@ -29,6 +29,28 @@ private:
     std::uniform_int_distribution<uint8_t> randomByte;
 
 private:
+    // Vx, Vy = register
+    // n = nibble
+    // nnn = memory addr
+    // kk = byte
+    // I = Index
+    // {,} = optional/ignored
+    // DT = Delay Timer
+    // ST = Sound Timer
+    // F = font sprite addr
+    // B = Binary-Coded Decimal (BCD)
+    // [] = memory addr itself
+
+    // JP = JUMP
+    // SE = Skip (next instruction) Equal
+    // SNE = Skip Not Equal
+    // LD = Load
+    // SHR = Shift right
+    // SUBN = reverse SUB
+    // SHL = Shift Left
+    // SKP = Skip key pressed
+    // SKNP = Skip Key not pressed
+    
     void OP_00E0(); // CLS
     void OP_00EE(); // RET
     void OP_1nnn(); // JP addr
@@ -36,31 +58,31 @@ private:
     void OP_3xkk(); // SE Vx, byte
     void OP_4xkk(); // SNE Vx, byte
     void OP_5xy0(); // SE Vx, Vy
-    void OP_6xkk(); // SNE Vx, byte
-    void OP_7xkk(); // ADD Vx, byte
+    void OP_6xkk(); // LD Vx, byte
+    void OP_7xkk(); // ADD Vx, byte 
     void OP_8xy0(); // LD Vx, Vy
     void OP_8xy1(); // OR Vx, Vy
     void OP_8xy2(); // AND Vx, Vy
     void OP_8xy3(); // XOR Vx, Vy
-    void OP_8xy4(); // ADD Vx, Vy
-    void OP_8xy5(); // SUB Vx, Vy
-    void OP_8xy6(); // SHR Vx
-    void OP_8xy7(); // SUBN Vx, Vy
-    void OP_8xyE(); // SHL Vx {, Vy}
+    void OP_8xy4(); // ADD Vx, Vy (VF = carry)
+    void OP_8xy5(); // SUB Vx, Vy (VF = !borrow)
+    void OP_8xy6(); // SHR Vx (VF = least significant bit)
+    void OP_8xy7(); // SUBN Vx, Vy (VF = !borrow)
+    void OP_8xyE(); // SHL Vx {, Vy} (VF = most significant bit)
     void OP_9xy0(); // SNE Vx, Vy
     void OP_Annn(); // LD I, addr
-    void OP_Bnnn(); // JP V0, addr
-    void OP_Cxkk(); // RND Vx, byte
-    void OP_Dxyn(); // DRW Vx, Vy, nibble
-    void OP_Ex9E(); // SKP Vx
+    void OP_Bnnn(); // JP V0, addr (addr + v0)
+    void OP_Cxkk(); // RND Vx, byte (Vx = Random&byte)
+    void OP_Dxyn(); // DRW Vx, Vy, nibble (draw nibble-byte sprite at mem-location I at (Vx, Vy), (VF = collision)
+    void OP_Ex9E(); // SKP Vx 
     void OP_ExA1(); // SKNP Vx
     void OP_Fx07(); // LD Vx, DT
     void OP_Fx0A(); // LD Vx, K
     void OP_Fx15(); // LD DT, Vx
     void OP_Fx18(); // LD ST, Vx
     void OP_Fx1E(); // ADD I, Vx
-    void OP_Fx29(); // LD F, Vx
+    void OP_Fx29(); // LD F, Vx 
     void OP_Fx33(); // LD B, Vx
-    void OP_Fx55(); // LD [I], Vx
-    void OP_Fx65(); // LD Vx, [I]
+    void OP_Fx55(); // LD [I], Vx (write to memory from I to I+X)
+    void OP_Fx65(); // LD Vx, [I] (read from memory fro I to I+X)
 };
