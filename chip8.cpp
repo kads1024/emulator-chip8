1a6a44124a7528dad822326eb43423f8f96b1345
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2026-09-01 10:28:12 +0800

    Fill function pointer tables

--- chip8.cpp
+++ chip8.cpp
@@ -36,10 +36,61 @@ chip8::chip8() : randomNumberGenerator(std::chrono::system_clock::now().time_sin
     }
 
     // Fill msater table
+    masterTable[0x0000u >> 12] = &chip8::Table0;
+    masterTable[0x1000u >> 12] = &chip8::OP_1nnn;
+    masterTable[0x2000u >> 12] = &chip8::OP_2nnn;
+    masterTable[0x3000u >> 12] = &chip8::OP_3xkk;
+    masterTable[0x4000u >> 12] = &chip8::OP_4xkk;
+    masterTable[0x5000u >> 12] = &chip8::OP_5xy0;
+    masterTable[0x6000u >> 12] = &chip8::OP_6xkk;
+    masterTable[0x7000u >> 12] = &chip8::OP_7xkk;
+    masterTable[0x8000u >> 12] = &chip8::Table8;
+    masterTable[0x9000u >> 12] = &chip8::OP_9xy0;
+    masterTable[0xA000u >> 12] = &chip8::OP_Annn;
+    masterTable[0xB000u >> 12] = &chip8::OP_Bnnn;
+    masterTable[0xC000u >> 12] = &chip8::OP_Cxkk;
+    masterTable[0xD000u >> 12] = &chip8::OP_Dxyn;
+    masterTable[0xE000u >> 12] = &chip8::TableE;
+    masterTable[0xF000u >> 12] = &chip8::TableF;
 
     // prefill subtable with OP_NULL
+    for (uint8_t table08EsubTableIndex = 0; table08EsubTableIndex < 0x000E + 1; table08EsubTableIndex++)
+    {
+        table0[table08EsubTableIndex] = OP_NULL;
+        table8[table08EsubTableIndex] = OP_NULL;
+        tableE[table08EsubTableIndex] = OP_NULL;
+    }
+    for (uint8_t tableFSubTableIndex = 0; tableFSubTableIndex < 0x0065 + 1; tableFSubTableIndex++)
+    {
+        tableF[tableFSubTableIndex] = OP_NULL;
+    }
 
     // fill correct subtable slots
+    table0[0x0000u] = &chip8::OP_00E0;
+    table0[0x000Eu] = &chip8::OP_00EE;
+
+    table8[0x0000u] = &chip8::OP_8xy0;
+    table8[0x0001u] = &chip8::OP_8xy1;
+    table8[0x0002u] = &chip8::OP_8xy2;
+    table8[0x0003u] = &chip8::OP_8xy3;
+    table8[0x0004u] = &chip8::OP_8xy4;
+    table8[0x0005u] = &chip8::OP_8xy5;
+    table8[0x0006u] = &chip8::OP_8xy6;
+    table8[0x0007u] = &chip8::OP_8xy7;
+    table8[0x000Eu] = &chip8::OP_8xyE;
+
+    tableE[0x000Eu] = &chip8::OP_Ex9E;
+    tableE[0x0001u] = &chip8::OP_ExA1;
+
+    tableF[0x0007u] = &chip8::OP_Fx07;
+    tableF[0x000Au] = &chip8::OP_Fx0A;
+    tableF[0x0015u] = &chip8::OP_Fx15;
+    tableF[0x0018u] = &chip8::OP_Fx18;
+    tableF[0x001Eu] = &chip8::OP_Fx1E;
+    tableF[0x0029u] = &chip8::OP_Fx29;
+    tableF[0x0033u] = &chip8::OP_Fx33;
+    tableF[0x0055u] = &chip8::OP_Fx55;
+    tableF[0x0065u] = &chip8::OP_Fx65;
 }
 
 void chip8::load_rom(const char* const fileName)
